105f4ea60829d7ee67e0b79741142f24ac34c5e5
Brian Morrison II <brian@brianmorrison.me>
2024-11-27 16:50:58 -0600

    add copy profile btn

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -3,8 +3,20 @@
 import Link from "next/link";
 import { UserButton, SignedOut, SignInButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { Copy } from "lucide-react";
+import { useUser } from "@clerk/nextjs";
+import { toast } from "sonner";
 
 export function Navbar() {
+  const { user } = useUser();
+  const copyPath = () => {
+    const path = `${window.location.origin}/${user?.username}`;
+    navigator.clipboard.writeText(path);
+    toast.success("Link copied!", {
+      description: `Profile link has been copied to your clipboard.`,
+    });
+  };
+
   return (
     <nav className="border-b">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -31,6 +43,13 @@ export function Navbar() {
             <SignedOut>
               <SignInButton mode="modal" />
             </SignedOut>
+            <button
+              onClick={copyPath}
+              className="flex items-center gap-2 rounded-md px-3 py-2 text-sm hover:bg-white hover:bg-opacity-90"
+            >
+              <Copy className="h-4 w-4" />
+              Share profile
+            </button>
             <UserButton />
           </div>
         </div>
